c0325e0b7b1337c2e178dd3e081ead2844f116f4
kevin73633 <kevindeepak@hotmail.com>
2025-06-30 17:45:51 +0800

    hopefully it works now

--- sc-automation/src/test/java/com/example/selenium/SanityTest.java
+++ sc-automation/src/test/java/com/example/selenium/SanityTest.java
@@ -1,9 +1,10 @@
 package com.example.selenium;
 
 import org.testng.annotations.Test;
-    public class SanityTest {
-        @Test(groups = {"Test123"})
-        public void sanityCheck() {
-            System.out.println("✅ Sanity test is running!");
-        }
+
+public class SanityTest {
+    @Test
+    public void sanityCheck() {
+        System.out.println("✅ Sanity test is working!");
     }
+}
