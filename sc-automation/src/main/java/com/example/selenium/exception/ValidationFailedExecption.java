f1c88f6345e267e7e2f6330e0a65158f8bb5186e
qcjiangqianchen <qc.jiang.2023@scis.smu.edu.sg>
2025-06-30 11:21:03 +0800

    exception files

---
+++ sc-automation/src/main/java/com/example/selenium/exception/ValidationFailedExecption.java
@@ -0,0 +1,8 @@
+package com.example.selenium.exception;
+
+public class ValidationFailedExecption extends Exception {
+    
+    public ValidationFailedExecption(String message) {
+        super(message);
+    }
+}
