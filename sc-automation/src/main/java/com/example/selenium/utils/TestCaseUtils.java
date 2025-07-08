6f1178b9e3763e7817c63959715454c1077f5dce
kevin73633 <kevindeepak@hotmail.com>
2025-07-08 12:44:11 +0800

    Create TestCaseUtils.java

---
+++ sc-automation/src/main/java/com/example/selenium/utils/TestCaseUtils.java
@@ -0,0 +1,31 @@
+package com.example.selenium.utils;
+
+import com.example.selenium.driver.DriverInstance;
+import com.example.selenium.utils.SeleniumUtils;
+
+import java.util.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestCaseUtils {
+    public void filterByLevelAndClass(String levelString, String classString) throws Exception {   
+        //navigate to level nav tab
+        SeleniumUtils.clickElement(By.xpath("//a[@class='site-menu-btn' and @data-target='#megaMenu-level']"));
+        Thread.sleep(2000); // Wait for the page to load
+        System.out.println("✅ level nav tab accessed");
+
+        //filter by level
+        SeleniumUtils.clickElement(By.xpath("//li[contains(@class, 'ng-star-inserted') and contains(text(), '" + levelString + "')]"));
+        Thread.sleep(2000); // Wait for the page to load
+        System.out.println("✅ level chosen"); 
+
+        //filter by class
+        SeleniumUtils.clickElement(By.xpath("//a[normalize-space(text())='" + classString + "']"));
+        Thread.sleep(2000); // Wait for the page to load
+        System.out.println("✅ class chosen");
+    }
+}
+    
